fdd4abd59682522de12202beffdbdf353f71e6a4
Hrodvitnir-Fenrir <leandrebaudouin@gmail.com>
2025-04-14 12:47:37 +0200

    init db

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const path = require("path");
+const { db, initDatabase } = require("./lib/db.js");
 
 const app = express();
 const PORT = 4000;
 
+initDatabase();
+
 // Sert les fichiers HTML/CSS/JS depuis le dossier public
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
 
 // Si tu as une page d'accueil en public/index.html
 app.get("/", (req, res) => {
