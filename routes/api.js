dcb862c9cf5ac8be33a258be769ab82f78e402da
Hrodvitnir-Fenrir <leandrebaudouin@gmail.com>
2025-04-16 22:46:44 +0200

    fetch google book for data

--- routes/api.js
+++ routes/api.js
@@ -12,4 +12,24 @@ router.post("/acc", (req, res) => {
 	}
 });
 
+router.get("/fetchInfo", async (req, res) => {
+	try {
+		const response = await fetch(
+			`https://www.googleapis.com/books/v1/volumes?q=isbn:${req.query.ean}`,
+		);
+		const responseData = await response.json();
+		const data = responseData.items?.[0];
+
+		if (!data) {
+			return res
+				.status(404)
+				.json({ success: false, message: "Livre non trouvé" });
+		}
+		res.status(200).json({ success: true, data });
+	} catch (err) {
+		console.error("Erreur fetchInfo :", err);
+		res.status(500).json({ success: false, message: "Erreur serveur" });
+	}
+});
+
 module.exports = router;
